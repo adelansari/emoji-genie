034986de202dcac82f375aed21cc3767f165c6d3
Alex Tran <tranduclong092@gmail.com>
2025-04-19 14:50:20 +0300

    Init: create a simple Konvas Canvas and joystick controller to position the emoji

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+import EmojiCanvas from "./components/EmojiCanvas"
+import JoystickController from "./components/JoystickController"
 
 function App() {
+  const [position, setPosition] = useState({
+    x: 300,
+    y: 300,
+  });
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+    <div className="min-h-screen flex flex-col items-center justify-center p-4 bg-gray-800">
+      <h1 className="text-4xl font-bold mb-8 text-yellow-400 tracking-wider">
+        Emoji Genie
       </h1>
-    </>
+      <div className="w-full max-w-6xl flex flex-col md:flex-row justify-center items-start gap-8">
+        <div className="flex-shrink-0">
+          <EmojiCanvas position={position} />
+        </div>
+        <div className="flex-shrink-0">
+          <JoystickController setPosition={setPosition} position={position} />
+        </div>
+      </div>
+    </div>
   )
 }
 
